d2bcc8aa6d10d0f5138832d8977b665b0f436d00
sonofjack2018 <jackssonnn@gmail.com>
2020-05-01 13:03:42 -0500

    Cloudinary updated

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,9 +7,9 @@ const CLOUDINARY_SECRET1 = '7bChKulxDSPFbLgqSxIsSv3GcewNew';
 const router = express.Router();
 
 cloudinary.config({ 
-  cloud_name: CLOUDINARY_NAME1, 
-  api_key: CLOUDINARY_KEY1, 
-  api_secret: CLOUDINARY_SECRET1 
+  cloud_name: df0thbo0x, 
+  api_key: 567262439544376, 
+  api_secret: -XB0cSViyqyy7Kztilh5adWLibs 
   //cloud_name: process.env.CLOUDINARY_NAME, 
   //api_key: process.env.CLOUDINARY_KEY, 
   //api_secret: process.env.CLOUDINARY_SECRET
